8e045103e2378db9acfe08ec18db6bbfb761348b
aprrenard <aprrenard@protonmail.com>
2024-03-19 15:11:57 +0100

    Added non longitudinal processing to suite2p script.

--- src/preprocessing/run_suite2p.py
+++ src/preprocessing/run_suite2p.py
@@ -1,15 +1,14 @@
 import os
 import sys
 
-import numpy as np
-
 from suite2p import run_s2p, default_ops
 sys.path.append('H://anthony//repos//fast-learning//src')
 import server_path
 
 
-mice_ids = ['AR129', 'AR131']
+mice_ids = ['RD046']
 experimenter = 'AR'
+longitudinal = False
 
 # set your options for running
 ops = default_ops() # populates ops with the default options
@@ -22,27 +21,58 @@ ops['delete_bin'] = True
 ops['reg_tif'] = True
 
 dbs = []
-for mouse_id in mice_ids:
-  tiff_folders = os.path.join(server_path.get_data_folder(), mouse_id, 'Recording', 'Imaging')
-  tiff_folders = [os.path.join(tiff_folders, folder) for folder in os.listdir(tiff_folders)
-                  if os.path.isdir(os.path.join(tiff_folders, folder))]
-  # tiff_folders = ['D://AR//test_data']
-  fast_disk = os.path.join('D:', 'suite2p', mouse_id)
-  save_path = os.path.join(server_path.get_experimenter_analysis_folder(experimenter),
-                           mouse_id)
-  if not os.path.exists(fast_disk):
-     os.mkdir(fast_disk)
+if longitudinal:
+    # Concatenate all sessions and run suite2p once per mouse.
+    for mouse_id in mice_ids:
+        tiff_folders = os.path.join(server_path.get_data_folder(), mouse_id, 'Recording', 'Imaging')
+        tiff_folders = [os.path.join(tiff_folders, folder) for folder in os.listdir(tiff_folders)
+                        if os.path.isdir(os.path.join(tiff_folders, folder))]
+        # tiff_folders = ['D://AR//test_data']
+        fast_disk = os.path.join('D:', 'suite2p', mouse_id)
+        save_path = os.path.join(server_path.get_experimenter_analysis_folder(experimenter),
+                                 mouse_id)
+        if not os.path.exists(fast_disk):
+            os.mkdir(fast_disk)
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
 
-    # db overwrites any ops (allows for experiment specific settings)
-    db = {
-        'h5py': [], # a single h5 file path
-        'h5py_key': 'data',
-        'data_path': tiff_folders, # a list of folders with tiffs
-                                                # (or folder of folders with tiffs if look_one_level_down is True, or subfolders is not empty)
-        'fast_disk': fast_disk, # string which specifies where the binary file will be stored (should be an SSD)
-        'save_path0': save_path,
-        }
-    dbs.append(db)
-
+        # db overwrites any ops (allows for experiment specific settings)
+        db = {
+            'h5py': [], # a single h5 file path
+            'h5py_key': 'data',
+            'data_path': tiff_folders, # a list of folders with tiffs
+                                                    # (or folder of folders with tiffs if look_one_level_down is True, or subfolders is not empty)
+            'fast_disk': fast_disk, # string which specifies where the binary file will be stored (should be an SSD)
+            'save_path0': save_path,
+            }
+        dbs.append(db)
+else:
+    # Run suite2p for each session of each mouse.
+    for mouse_id in mice_ids:
+        tiff_folders = os.path.join(server_path.get_data_folder(), mouse_id, 'Recording', 'Imaging')
+        tiff_folders = [os.path.join(tiff_folders, folder)
+                        for folder in os.listdir(tiff_folders)
+                        if os.path.isdir(os.path.join(tiff_folders, folder))]
+        
+        for folder in tiff_folders:
+            session_id = os.path.split(folder)[1]
+            fast_disk = os.path.join('D:', 'suite2p', mouse_id, session_id)
+            save_path = os.path.join(server_path.get_experimenter_analysis_folder(experimenter),
+                                     mouse_id, session_id)
+            if not os.path.exists(fast_disk):
+                os.makedirs(fast_disk)
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+            # db overwrites any ops (allows for experiment specific settings)
+            db = {
+                'h5py': [], # a single h5 file path
+                'h5py_key': 'data',
+                'data_path': [folder], # a list of folders with tiffs
+                                                        # (or folder of folders with tiffs if look_one_level_down is True, or subfolders is not empty)
+                'fast_disk': fast_disk, # string which specifies where the binary file will be stored (should be an SSD)
+                'save_path0': save_path,
+                }
+            dbs.append(db)
+    
 for dbi in dbs:
     opsEnd = run_s2p(ops=ops, db=dbi)
