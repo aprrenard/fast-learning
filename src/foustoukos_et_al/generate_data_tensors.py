b607493a3a4f07b7a3ff602173a91bfd2f68ae52
Anthony Renard <aprrenard@protonmail.com>
2024-07-25 16:35:19 +0200

    PSTH for all trial types.

---
+++ src/foustoukos_et_al/generate_data_tensors.py
@@ -0,0 +1,319 @@
+"""This script generates PSTH numpy arrays from lists of NWB files.
+"""
+
+import os
+import pickle
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import yaml
+
+# sys.path.append('H:\\anthony\\repos\\NWB_analysis')
+import nwb_utils.utils_io as io
+from analysis.psth_analysis import make_events_aligned_array_6d
+from nwb_wrappers import nwb_reader_functions as nwb_read
+
+
+# =============================================================================
+# Make activity 6d array of sensory mapping trials (non-motivated trials).
+# =============================================================================
+
+group_yaml = r"C:/Users/aprenard/recherches/repos/fast-learning/docs/groups/imaging_non_rewarded.yaml"
+trial_indices_yaml = r"C:/Users/aprenard/recherches/repos/fast-learning/docs/stop_flags/trial_indices_sensory_map.yaml"
+processed_data_dir = (r'//sv-nas1.rcp.epfl.ch/Petersen-Lab/analysis/'
+                      r'Anthony_Renard/data_processed/')
+dataset_name = 'psth_sensory_map_trials_non_rewarded.npy'
+metadata_name = 'psth_sensory_map_trials_non_rewarded.pickle'
+
+with open(group_yaml, 'r') as stream:
+    nwb_list = yaml.safe_load(stream)
+with open(trial_indices_yaml, 'r') as stream:
+    trial_indices = yaml.load(stream, yaml.Loader)
+trial_indices = pd.DataFrame(trial_indices.items(), columns=['session_id', 'trial_idx'])
+# trial_indices = None
+trial_selection = {'whisker_stim': [1], 'lick_flag':[0]}
+cell_types = ['na', 'wM1', 'wS2']
+rrs_keys = ['ophys', 'fluorescence_all_cells', 'dff']
+time_range = (1,3)
+epoch_name = None
+
+traces, metadata = make_events_aligned_array(nwb_list, rrs_keys,
+                                             time_range, trial_selection,
+                                             epoch_name, cell_types,
+                                             trial_indices)
+
+# Save dataset.
+save_path = os.path.join(processed_data_dir, dataset_name)
+np.save(save_path, traces)
+save_path = os.path.join(processed_data_dir, metadata_name)
+# metadata['cell_types'] = list(metadata['cell_types'])
+with open(save_path, 'wb') as fid:
+    pickle.dump(metadata, fid)
+
+
+# =============================================================================
+# Make activity 6d array of each trial type (motivated).
+# =============================================================================
+
+# Rewarded and non-rewarded NWB files.
+group_yaml_rew = r"C:/Users/aprenard/recherches/repos/fast-learning/docs/groups/imaging_rewarded.yaml"
+group_yaml_non_rew = r"C:/Users/aprenard/recherches/repos/fast-learning/docs/groups/imaging_non_rewarded.yaml"
+
+# Stop flags for each session.
+trial_indices_yaml = r"C:/Users/aprenard/recherches/repos/fast-learning/docs/stop_flags/trial_indices_end_session.yaml"
+
+processed_data_dir = (r'//sv-nas1.rcp.epfl.ch/Petersen-Lab/analysis/'
+                      r'Anthony_Renard/data_processed/')
+
+dataset_names = ['psth_WH.npy',
+                 'psth_WM.npy',
+                 'psth_AH.npy',
+                 'psth_AM.npy',
+                 'psth_FA.npy',
+                 'psth_CR.npy',
+                 ]
+metadata_names = ['psth_WH_metadata.pickle',
+                 'psth_WM_metadata.pickle',
+                 'psth_AH_metadata.pickle',
+                 'psth_AM_metadata.pickle',
+                 'psth_FA_metadata.pickle',
+                 'psth_CR_metadata.pickle',
+                 ]
+trial_selections = [{'whisker_stim': [1], 'lick_flag':[1]},
+                    {'whisker_stim': [1], 'lick_flag':[0]},
+                    {'auditory_stim': [1], 'lick_flag':[1]},
+                    {'auditory_stim': [1], 'lick_flag':[0]},
+                    {'no_stim': [1], 'lick_flag':[1]},
+                    {'no_stim': [1], 'lick_flag':[0]},
+                    ]
+
+with open(group_yaml_rew, 'r') as stream:
+    nwb_list_rew = yaml.safe_load(stream)
+with open(group_yaml_non_rew, 'r') as stream:
+    nwb_list_non_rew = yaml.safe_load(stream)
+nwb_list = nwb_list_rew + nwb_list_non_rew
+
+with open(trial_indices_yaml, 'r') as stream:
+    trial_indices = yaml.load(stream, yaml.Loader)
+trial_indices = pd.DataFrame(trial_indices.items(), columns=['session_id', 'trial_idx'])
+
+# Parameters for PSTH.
+cell_types = ['na', 'wM1', 'wS2']
+rrs_keys = ['ophys', 'fluorescence_all_cells', 'dff']
+time_range = (2,5)
+epoch_name = None
+
+# Loop on each trial type and save the dataset.
+for dataset_name, metadata_name, trial_selection in zip(dataset_names,
+                                                        metadata_names,
+                                                        trial_selections):
+    print(f'Processing {dataset_name}')
+    traces, metadata = make_events_aligned_array_6d(nwb_list, rrs_keys,
+                                                 time_range, trial_selection,
+                                                 epoch_name, cell_types,
+                                                 trial_indices)
+    # Save dataset.
+    save_path = os.path.join(processed_data_dir, dataset_name)
+    np.save(save_path, traces)
+    save_path = os.path.join(processed_data_dir, metadata_name)
+    # metadata['cell_types'] = list(metadata['cell_types'])
+    with open(save_path, 'wb') as fid:
+        pickle.dump(metadata, fid)
+
+
+data_path =r"\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Anthony_Renard\data_processed\psth_WH.npy"
+metadata_path = r"\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Anthony_Renard\data_processed\psth_WH_metadata.pickle"
+with open(metadata_path, 'rb') as fid:
+    metadata = pickle.load(fid)
+data = np.load(data_path)
+
+
+
+# # Make pandas dataset to check that I have the same than with numpy.
+# # ##################################################################
+
+# trial_idx = trial_idx_table_GF
+# trial_selection = {'whisker_stim': [1], 'lick_flag':[0]}
+# cell_types = ['na', 'wM1', 'wS2']
+
+# traces_rew_pd = make_events_aligned_data_table(nwb_list_rew, rrs_keys, time_range,
+#                                                 trial_selection, epoch_name, True, trial_idx)
+# traces_non_rew_pd = make_events_aligned_data_table(nwb_list_non_rew, rrs_keys, time_range,
+#                                                 trial_selection, epoch_name, True, trial_idx)
+
+# stop_flag = {session_id: trial_idx_table_GF.loc[trial_idx_table_GF.session_id==session_id, 'trial_idx'].values[0]
+#              for session_id in trial_idx_table_GF.session_id.unique()}
+# save_path = '\\\\sv-nas1.rcp.epfl.ch\\Petersen-Lab\\analysis\\Anthony_Renard\\data_processed\\stop_flag_GF.yaml'
+# with open(save_path, 'w') as outfile:
+#     yaml.safe_dump(stop_flag, outfile)
+
+# with open(save_path, 'r') as fid:
+#     test = yaml.safe_load(fid)
+
+# trial_idx_table_GF.loc[trial_idx_table_GF.session_id=='GF264_02072020_104646', 'trial_idx'].to_list()
+
+
+# # traces_non_rew_pd.loc[(traces_non_rew_pd.mouse_id.isin(['GF208']))].cell_type.unique()
+
+# # # traces_non_rew_pd.loc[(traces_non_rew_pd.behavior_day.isin([0])) & (d.roi==0) & (d.event==3)]
+# # event = 0
+# # d = traces_non_rew_pd.loc[(traces_non_rew_pd.mouse_id.isin(['GF208']))
+# #                           &  (traces_non_rew_pd.behavior_day.isin([0]))
+# #                           & (traces_non_rew_pd.roi==0)
+# #                           & (traces_non_rew_pd.event==event)]
+# # plt.figure()
+# # plt.plot(d.activity.to_numpy())
+
+# # traces_non_rew_np.shape
+# # psth = traces_non_rew_np[0,2,0,0,event]
+# # plt.plot(psth.flatten())
+
+
+
+# # # Compare with data from nwb file.
+# # nwb_file = nwb_list_non_rew[2]
+# # rrs = nwb_read.get_roi_response_serie_data(nwb_file, ['ophys', 'fluorescence_all_cells', 'dff'])
+# # table = nwb_read.get_trial_table(nwb_file)
+
+# # trial_idx_table_GF.loc[trial_idx_table_GF.session_id=='GF208_02102019_094122', 'trial_idx'].values[0]
+
+# # event = 769
+
+# # start_time = table.loc[table.trial_id==event].start_time.values[0]
+# # rrs_ts = nwb_read.get_roi_response_serie_timestamps(nwb_file, ['ophys', 'fluorescence_all_cells', 'dff'])
+
+# # central_frame = find_nearest(rrs_ts, start_time+2)
+# # nwb_d = rrs[0,central_frame-30:central_frame+90]
+# # nwb_d -= np.mean(nwb_d[0:30], axis=0)
+
+# # plt.figure()
+# # plt.plot(nwb_d)
+
+
+# d = traces_rew_pd.loc[traces_rew_pd.behavior_day.isin([-1,1])]
+# d = d.groupby(['mouse_id','session_id', 'behavior_type', 'behavior_day', 'roi', 'time'], as_index=False).agg({'activity':np.nanmean})
+# # d = d.groupby(['behavior_day', 'time'], as_index=False).agg({'activity':np.nanmean})
+# plt.figure()
+# # plt.plot(np.mean(d.activity.to_numpy().reshape((2,121)), axis=0))
+# # plt.plot(d.activity)
+# sns.lineplot(data=d, x='time', y="activity", hue='behavior_day', errorbar=None, estimator=np.nanmean)
+
+# d = traces_non_rew_pd.loc[traces_non_rew_pd.behavior_day.isin([-1,1])]
+# d = d.groupby(['mouse_id','session_id', 'behavior_type', 'behavior_day', 'roi', 'time'], as_index=False).agg({'activity':np.nanmean})
+# # d = d.groupby(['behavior_day', 'time'], as_index=False).agg({'activity':np.nanmean})
+# plt.figure()
+# # plt.plot(np.mean(d.activity.to_numpy().reshape((2,121)), axis=0))
+# # plt.plot(d.activity)
+# sns.lineplot(data=d, x='time', y="activity", hue='behavior_day', errorbar=None, estimator=np.nanmean)
+
+# psth = np.nanmean(traces_non_rew_np, axis=(4))
+# days = np.nanmean(psth, axis=(0,2,3))
+
+# plt.figure()
+# plt.plot(days[0])
+# plt.plot(days[1])
+# plt.plot(days[2])
+# plt.plot(days[3])
+# plt.plot(days[4])
+
+
+
+# # Checking that data correspond for GF.
+# # #####################################
+
+# import json
+
+# nwb_file = r'\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Anthony_Renard\NWB\AR144_20240518_193553.nwb'
+# rrs_fissa = nwb_read.get_roi_response_serie_data(nwb_file, ['ophys', 'fluorescence_all_cells', 'F_cor'])
+# rrs_dff = nwb_read.get_roi_response_serie_data(nwb_file, ['ophys', 'fluorescence_all_cells', 'dff'])
+# rrs_dff.shape
+# fissa_file = r"\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Georgios_Foustoukos\FISSASessionData\GF264\GF264_02072020\F_fissa.npy"
+# baseline_file = r"\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Georgios_Foustoukos\Baselines\GF264\GF264_02072020\baselines.npy"
+# dff_json = r"\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Georgios_Foustoukos\FoustoukosData\Data\GF264\Recordings\CalciumData\GF264_02072020_104646\trial_0252\dffRois.json"
+# fissa = np.load(fissa_file, allow_pickle=True)
+# f0 = np.load(baseline_file, allow_pickle=True)
+# with open(dff_json, 'r') as stream:
+#     dff = json.load(stream)
+# dff = np.array(dff)
+
+# result_file = r'\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Georgios_Foustoukos\FoustoukosData\Data\GF264\Recordings\BehaviourFiles\GF264_02072020_104646\Results.txt'
+# df = pd.read_csv(result_file, sep=r'\s+', engine='python')
+# df.trial_number
+# df.loc[df.Perf==6]
+# df.loc[df.Perf!=6]
+
+# plt.figure()
+# plt.plot(np.mean(rrs_dff[:,-303:], axis=0))
+# plt.plot(np.mean(dff[:,:], axis=0))
+
+# plt.figure()
+# plt.plot(np.mean(rrs_dff[:,-303:], axis=0)[30:150])
+# plt.plot(np.mean(dff[:,:], axis=0)[30:150])
+
+
+
+# plt.plot(rrs_dff[0])
+# trial_table = nwb_read.get_trial_table(nwb_file)
+# trial_table.trial_id
+
+# ts = nwb_read.get_roi_response_serie_timestamps(nwb_file, ['ophys', 'fluorescence_all_cells', 'dff'])
+# ts.shape
+# rrs_dff.shape
+
+# ts[-303]
+# # last frame 3404.8333333333335
+
+# traces_rew
+
+# # Flatten mean response across cells
+# a = np.nanmean(traces_rew[1,1,:,:,:52,:], axis=(0,1))
+# a.shape
+# a = a.flatten()
+# b = np.nanmean(rrs_dff, axis=0)
+
+# f, axes = plt.subplots(2,1)
+# axes[0].plot(b[-303:])
+# axes[1].plot(a[-120:])
+
+
+# a.shape
+# b.shape
+# rrs_dff.shape
+
+# metadata_rew
+
+# traces_rew.shape
+# np.sum(~(np.isnan(traces_rew[1,1,0,0,:,0])))
+# np.sum(~(np.isnan(traces_rew[1,1,:,:,0,0])))
+
+
+
+# path = r"\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Anthony_Renard\data\AR144\suite2p\plane0\F.npy"
+# d = np.load(path)
+
+# plt.plot(d[0])
+# # 3260.900000
+
+
+# # # ---------------
+# # import yaml
+# # import json
+# # path = 'C:\\Users\\aprenard\\Downloads\\TrialStop-20240510T122435Z-001\\TrialStop'
+# # save_yaml = 'C:\\Users\\aprenard\\recherches\\repos\\fast-learning\\docs\\stop_flag_GF.yaml'
+# # stop_flags = []
+
+# # mice = os.listdir(path)
+# # for mouse in mice:
+# #     mouse_path = os.path.join(path, mouse)
+# #     sessions = os.listdir(mouse_path)
+# #     for session in sessions:
+# #         session_id = session[:-5]
+# #         json_path = os.path.join(mouse_path, session)
+# #         with open(json_path) as f:
+# #             d = json.load(f)
+# #             stop_flags.append([session_id, d])
+# # with open(save_yaml, 'w') as stream:
+# #     yaml.dump(stop_flags, stream)
--- src/foustoukos_et_al/make_yaml_stop_flag.py
+++ src/foustoukos_et_al/make_yaml_stop_flag.py
@@ -1,99 +1,155 @@
-"""This script generate yaml files associating a stop trial index ti each session to know when to cut the session.
-
+"""This script generates yaml files associating a (start, stop) tuple trial
+index for each session to know when to cut the session.
 """
 import os
 
 import pandas as pd
+import matplotlib.pyplot as plt
 import yaml
 
 from nwb_wrappers import nwb_reader_functions as nwb_read
+from src.utils.utils_io import read_excel_db
 
 
-# Read NWB directory and list all NWB files.
+# =============================================================================
+# Path configuation.
+# =============================================================================
+
 nwb_path = r'\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Anthony_Renard\NWB'
-nwb_list = os.listdir(nwb_path)
-nwb_list = [os.path.join(nwb_path,f) for f in nwb_list]
+excel_path = r'\\sv-nas1.rcp.epfl.ch\Petersen-Lab\analysis\Anthony_Renard\mice_info\session_metadata.xlsx'
 
-# Generate stop flags for the whisker sensory mapping block presented at the end of the session
-# aka the non-motivated whisker trials.
-# In that case the stop flag is a beginning flag corresponding to the first trial of this block.
 
-stop_flag_non_motiv = {'stop_flag': {},
-                     'trial_indices': {}
-                     }
+# =============================================================================
+# Generate stop flags for the whisker sensory mapping block.
+# =============================================================================
+# Imaging sessions have a block of 50 whisker trials at the end of the session.
+# Find the trial index of the first whisker trial of this block.
+
+# List nwb files.
+nwb_list = read_excel_db(excel_path)
+nwb_list = nwb_list.loc[(nwb_list['exclude']!='exclude')]
+nwb_list = nwb_list.loc[(nwb_list['two_p_imaging'] == 'yes')
+                        & (nwb_list['sensory_mapping'] == 'yes')]
+nwb_list = list(nwb_list.session_id)
+nwb_list = [os.path.join(nwb_path, f + '.nwb') for f in nwb_list]
+
+stop_flags = {}
+trial_indices = {}
+trial_count = []
 
 for nwb_file in nwb_list:
+
     mouse_id = nwb_file[-25:-20]
     session_id = nwb_file[-25:-4]
     table = nwb_read.get_trial_table(nwb_file)
+    print(f"Processing {session_id} ({nwb_list.index(nwb_file) + 1}/{len(nwb_list)})")
 
-    # End of motivated block i.e. trial just before start of the 50 non-motivated trials.
-    non_mot_begin = (table.whisker_stim != table.whisker_stim.shift()).cumsum().idxmax()
-    stop_flag_non_motiv['stop_flag'][session_id] = non_mot_begin
-    # Exceptions. Forgot to remove the auditory trials.
+    # Beginning of the whisker sensory mapping block.
+    # Find final segment of one's in the whisker_stim column.
+    start = table.whisker_stim != table.whisker_stim.shift()
+    plt.plot(start.cumsum())
+    start = int(start.cumsum().idxmax())
+    # If more than 50 trial of whisker stimulation, keep only the last 50.
+    if table.loc[start:].shape[0] > 50:
+        start = start + table.loc[start:].shape[0] - 50
+    # Exceptions.
     if session_id == 'AR132_20240427_122605':
-        stop_flag_non_motiv['stop_flag'][session_id] = 571
+        start = 578
+    if session_id == 'AR139_20240428_180459':
+        start = 414
+    if session_id == 'GF350_28052021_145113':
+        start = 224
+    stop_flags[session_id] = (start, int(table.index.max()))
 
     # Get the indices of those trials.
-    non_mot_idx = table.loc[(table.trial_id>=non_mot_begin), 'trial_id'].to_list()
-    # Select only the 50 last non-motivated trials (in case more than 50 were presented).
-    non_mot_idx = non_mot_idx[-50:]
-    stop_flag_non_motiv['trial_indices'][session_id] = non_mot_idx
+    trial_ids = table.loc[start:, 'trial_id'].to_list()
+    trial_indices[session_id] = trial_ids
 
-# Save yaml file.
-yaml_save = r'C:\Users\aprenard\recherches\repos\fast-learning\docs\stop_flags\stop_flags_non_motiv.yaml'
+    # Count trial for sanity check.
+    n_wh_miss = table.loc[(table.trial_id >= start)
+                          & (table.whisker_stim == 1)
+                          & (table.lick_flag == 0)].shape[0]
+    n_wh_hit = table.loc[(table.trial_id >= start)
+                         & (table.whisker_stim == 1)
+                         & (table.lick_flag == 1)].shape[0]
+    trial_count.append([mouse_id, session_id, start, n_wh_miss, n_wh_hit])
+
+# Save yaml files.
+yaml_save = r'C:\Users\aprenard\recherches\repos\fast-learning\docs\stop_flags\stop_flags_sensory_map.yaml'
 with open(yaml_save, 'w') as stream:
-    yaml.safe_dump(stop_flag_non_motiv, stream)
+    yaml.safe_dump(stop_flags, stream)
+yaml_save = r'C:\Users\aprenard\recherches\repos\fast-learning\docs\stop_flags\trial_indices_sensory_map.yaml'
+with open(yaml_save, 'w') as stream:
+    yaml.safe_dump(trial_indices, stream)
+
+# # Sanity check.
+# trial_count = pd.DataFrame(trial_count, columns = ['mouse_id', 'session_id', 'start', 'n_wh_miss', 'n_wh_hit'])
+
+# nwb_file = '\\\\sv-nas1.rcp.epfl.ch\\Petersen-Lab\\analysis\\Anthony_Renard\\NWB\\AR115_20231116_142507.nwb'
+# table = nwb_read.get_trial_table(nwb_file)
+# table = table.reset_index()
+# plt.figure()
+# plt.scatter(table.loc[table.auditory_stim==1, 'trial_id'], table.loc[table.auditory_stim==1, 'lick_flag'])
+# plt.scatter(table.loc[table.whisker_stim==1, 'trial_id'], table.loc[table.whisker_stim==1, 'lick_flag'])
+# plt.scatter(table.loc[table.no_stim==1, 'trial_id'], table.loc[table.no_stim==1, 'lick_flag'])
 
 
+# =============================================================================
+# Generate session stop flags.
+# =============================================================================
+# The criteria to cut the session is the mouse is not performing the auditory
+# defined as three auditory misses in a row and no more than two auditory
+# hits in the rest.
 
+# List nwb files.
+nwb_list = read_excel_db(excel_path)
+nwb_list = nwb_list.loc[(nwb_list['exclude']!='exclude')]
+nwb_list = list(nwb_list.session_id)
+nwb_list = [os.path.join(nwb_path, f + '.nwb') for f in nwb_list]
 
-# Check selected trials with various stop criteria.
-
-temp = []
-trial_idx_table_GF = []
-trial_idx_table_AR = []
+stop_flags = {}
+trial_indices = {}
 
 for nwb_file in nwb_list:
 
     mouse_id = nwb_file[-25:-20]
     session_id = nwb_file[-25:-4]
     table = nwb_read.get_trial_table(nwb_file)
+    print(f"\rProcessing {session_id} ({nwb_list.index(nwb_file) + 1}/{len(nwb_list)})", end="")
 
-    # Specific of GF data.
-    if ('GF' in session_id) or ('MI' in session_id):
-        flag_trial = stop_flags_GF.loc[stop_flags_GF.session_id==session_id, 'stop_flag'].values[0]
-        n_wh_miss = table.loc[(table.trial_id>=flag_trial) & (table.whisker_stim == 1) & (table.lick_flag == 0)].shape[0]
-        n_wh_hit = table.loc[(table.trial_id>=flag_trial) & (table.whisker_stim == 1) & (table.lick_flag == 1)].shape[0]
+    # If a session does not contain auditory trials, keep whole session.
+    # This happens for some test sessions of the GF mice.
+    if table.auditory_stim.sum() == 0:
+        stop_flags[session_id] = (0, int(table.index.max()))
+        trial_indices[session_id] = table['trial_id'].to_list()
+        continue
 
-        # Get the indices of those trials.
-        non_mot_idx = table.loc[(table.trial_id>=flag_trial), 'trial_id'].to_list()
-        trial_idx_table_GF.append([session_id, non_mot_idx])
+    # Get index after which no more than two hits until session end.
+    two_hits_left = table.loc[table.auditory_stim==1, 'lick_flag']
+    two_hits_left = (two_hits_left.cumsum() >= (two_hits_left.sum() - 2)).idxmax()
+    # Find three auditory misses in a row.
+    three_aud_misses = table.loc[table.auditory_stim==1, 'lick_flag']
+    three_aud_misses = three_aud_misses.rolling(window=3).sum() == 0
+    # Stop flag is the first occurence of three auditory misses in a row
+    # followed by no more than three hits in the rest of the session.
+    if three_aud_misses.sum() == 0:
+        # In case no three auditory misses keep the whole session.
+        stop = table.index.max()
     else:
-        flag_trial = 'na'
-        n_wh_miss = 'na'
-        n_wh_hit = 'na'
-
-    # Check if it is different from taking the misses once the experimenter played wh trials only
-    # at the end of the session.
-    # End of motivated block i.e. trial just before start of the 50 non-motivated trials.
-    end_mot = (table.whisker_stim != table.whisker_stim.shift()).cumsum().idxmax() - 1
-    n_wh_miss_2 = table.loc[(table.trial_id>end_mot) & (table.whisker_stim == 1) & (table.lick_flag == 0)].shape[0]
-    n_wh_hit_2 = table.loc[(table.trial_id>end_mot) & (table.whisker_stim == 1) & (table.lick_flag == 1)].shape[0]
-
-    # Exceptions. Forgot to remove the auditory trials.
-    if session_id == 'AR132_20240427_122605':
-        end_mot = 570
-        n_wh_miss_2 = table.loc[(table.trial_id>end_mot) & (table.whisker_stim == 1) & (table.lick_flag == 0)].shape[0]
-        n_wh_hit_2 = table.loc[(table.trial_id>end_mot) & (table.whisker_stim == 1) & (table.lick_flag == 1)].shape[0]
+        stop = three_aud_misses.loc[three_aud_misses.index>two_hits_left].idxmax()
+    # Exceptions.
+    if session_id == 'AR115_20231116_142507':
+        stop = 35
+    stop_flags[session_id] = (0, stop)
 
     # Get the indices of those trials.
-    non_mot_idx = table.loc[(table.trial_id>end_mot), 'trial_id'].to_list()
-    # Select only the 50 last non-motivated trials (in case more than 50 were presented).
-    non_mot_idx = non_mot_idx[-50:]
-    trial_idx_table_AR.append([session_id, non_mot_idx])
+    trial_ids = table.loc[(table.index<=stop), 'trial_id'].to_list()
+    trial_indices[session_id] = trial_ids
 
-    temp.append([mouse_id, session_id, flag_trial, n_wh_miss, n_wh_hit, end_mot, n_wh_miss_2, n_wh_hit_2,])
-stop_flag_table = pd.DataFrame(temp, columns = ['mouse_id', 'session_id', 'stop_flag_GF', 'n_wh_miss_GF', 'n_wh_hit_GF', 'stop_flag_AR', 'n_wh_miss_AR', 'n_wh_hit_AR'])
-trial_idx_table_GF = pd.DataFrame(trial_idx_table_GF, columns=['session_id', 'trial_idx'])
-trial_idx_table_AR = pd.DataFrame(trial_idx_table_AR, columns=['session_id', 'trial_idx'])
+# Save yaml files.
+yaml_save = r'C:\Users\aprenard\recherches\repos\fast-learning\docs\stop_flags\stop_flags_end_session.yaml'
+with open(yaml_save, 'w') as stream:
+    yaml.dump(stop_flags, stream)
+yaml_save = r'C:\Users\aprenard\recherches\repos\fast-learning\docs\stop_flags\trial_indices_end_session.yaml'
+with open(yaml_save, 'w') as stream:
+    yaml.dump(trial_indices, stream)
