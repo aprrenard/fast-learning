29bd6c660029904b916f872d3f4db8f58630a973
Anthony Renard <aprrenard@protonmail.com>
2024-08-27 17:06:02 +0200

    Forgot utils.

--- src/utils/utils_io.py
+++ src/utils/utils_io.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pandas as pd
+import yaml
 
 
 def read_excel_db(db_path):
@@ -9,10 +10,21 @@ def read_excel_db(db_path):
     # Remove empty lines.
     database = database.loc[~database.isna().all(axis=1)]
 
-    # Change yes/no columns to booleans.
-    database = database.replace('yes', True)
-    database = database.replace('no', False)
-    database = database.astype({'two_p_imaging': bool,
-                                'optogenetic': bool,'pharmacology': bool})
-
     return database
+
+
+def read_group_yaml(group_yaml_path):
+    with open(group_yaml_path, 'r') as file:
+        group_yaml = yaml.safe_load(file)
+
+    return group_yaml
+
+
+def read_stop_flags_and_indices_yaml(stop_flag_yaml_path, trial_indices_path):
+    with open(stop_flag_yaml_path, 'r') as file:
+        stop_flags = yaml.safe_load(file)
+    with open(trial_indices_path, 'r') as file:
+        trial_indices = yaml.safe_load(file)
+    trial_indices = pd.DataFrame(trial_indices.items(), columns=['session_id', 'trial_idx'])
+    
+    return stop_flags, trial_indices
